b187e55d53d7aaed5ea145df8a6de89d2a0e40c4
sarvarimark <sarvari.mark@gmail.com>
2021-03-16 15:57:46 +0100

    Osztály fejlécek hozzáadása
    Első heti feladatok

---
+++ lib/utils/user.dart
@@ -0,0 +1,12 @@
+class User {
+  //Publikus változók - amik a specifikációban vannak
+
+  //Privát változók - amikre szerinted szükség van
+
+  //Setterek és getterek - amennyiben vannak validálandó publikus váltózóid
+
+  //Publikus függvények aka Interface - amit a specifikáció megkövetel
+
+  //Privát függvények - amikre szerinted szükség van
+
+}
