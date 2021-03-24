052cfb75cb7ace5b33c075f017e247eb33c06a45
sarvarimark <sarvari.mark@gmail.com>
2021-03-24 22:27:10 +0100

    Formázás

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -3,73 +3,82 @@ import '../models/preferences.dart';
 import 'io.dart';
 
 class Settings {
-
   //Privát változók
-  SharedPreferences? ownSharedPreferences;
-  Settings(){intialize();}
-  
+  SharedPreferences? _ownSharedPreferences;
+
+  //Konstruktor
+  Settings() {
+    _intialize();
+  }
+
   //Getterek
-  DarkMode get darkMode { 
-    var value = ownSharedPreferences!.getString('darkMode');
-    return DarkMode.values.firstWhere((element) => 
-    element.toString()=='DarkMode.$value');
+  DarkMode get darkMode {
+    var value = _ownSharedPreferences!.getString('darkMode');
+    return DarkMode.values
+        .firstWhere((element) => element.toString() == 'DarkMode.$value');
   }
 
-  Language get language { 
-    var value = ownSharedPreferences!.getString('language');
-    return Language.values.firstWhere((element) => 
-    element.toString()=='Language.$value');
+  Language get language {
+    var value = _ownSharedPreferences!.getString('language');
+    return Language.values
+        .firstWhere((element) => element.toString() == 'Language.$value');
   }
 
-  Theme get theme { 
-    var value = ownSharedPreferences!.getString('theme');
-    return Theme.values.firstWhere((element) => 
-    element.toString()=='Theme.$value');
+  Theme get theme {
+    var value = _ownSharedPreferences!.getString('theme');
+    return Theme.values
+        .firstWhere((element) => element.toString() == 'Theme.$value');
   }
 
-  bool? get dataLite => ownSharedPreferences!.getBool('dataLite');
+  bool? get dataLite => _ownSharedPreferences!.getBool('dataLite');
 
-  Map <String, bool>? get notifications {
-    var enabled = ownSharedPreferences!.getStringList('enabled');
-    var disabled = ownSharedPreferences!.getStringList('disabled');
+  Map<String, bool>? get notifications {
+    var enabled = _ownSharedPreferences!.getStringList('enabled');
+    var disabled = _ownSharedPreferences!.getStringList('disabled');
     var result = <String, bool>{};
-    
-    enabled!.forEach((element) {result.putIfAbsent(element, () => true);});
-    disabled!.forEach((element) {result.putIfAbsent(element, () => false);});
-    
+
+    enabled!.forEach((element) {
+      result.putIfAbsent(element, () => true);
+    });
+    disabled!.forEach((element) {
+      result.putIfAbsent(element, () => false);
+    });
+
     return result;
   }
 
   //Setterek
- set darkMode (DarkMode mode) { 
-    ownSharedPreferences!.setString('darkMode', mode.toString());
+  set darkMode(DarkMode mode) {
+    _ownSharedPreferences!.setString('darkMode', mode.toString());
   }
 
- set language (Language ownLanguage) { 
-    ownSharedPreferences!.setString('language', ownLanguage.toString());
+  set language(Language ownLanguage) {
+    _ownSharedPreferences!.setString('language', ownLanguage.toString());
   }
 
- set theme (Theme ownTheme) { 
-    ownSharedPreferences!.setString('theme', ownTheme.toString());
+  set theme(Theme ownTheme) {
+    _ownSharedPreferences!.setString('theme', ownTheme.toString());
   }
 
-  set dataLite (bool? ownLite){
+  set dataLite(bool? ownLite) {
     ownLite ??= false;
-    ownSharedPreferences!.setBool('dataLite', ownLite);
-  } 
+    _ownSharedPreferences!.setBool('dataLite', ownLite);
+  }
 
-  set notifications (Map<String,bool>? notif){
+  set notifications(Map<String, bool>? notif) {
     var enabled = <String>[];
     var disabled = <String>[];
-    for(var key in notif!.keys){
-      notif[key] == true? enabled.add(key): disabled.add(key);
-    }
-    ownSharedPreferences!.setStringList('disabled',disabled);
-    ownSharedPreferences!.setStringList('enabled',enabled);
-      }
 
-  //További publikus függvények (Interface)
-  bool loadPreferences(){
+    for (var key in notif!.keys) {
+      notif[key] == true ? enabled.add(key) : disabled.add(key);
+    }
+
+    _ownSharedPreferences!.setStringList('disabled', disabled);
+    _ownSharedPreferences!.setStringList('enabled', enabled);
+  }
+
+  //Publikus függvények (Interface)
+  bool loadPreferences() {
     var io = IO();
     var serverPreferences = io.getUserPreferences();
     darkMode = serverPreferences.darkMode;
@@ -80,20 +89,19 @@ class Settings {
     return true;
   }
 
-  bool savePreferences(){ 
-  var prefs = Preferences();
-  prefs.darkMode = darkMode; 
-  prefs.language = language;
-  prefs.theme = theme;
+  bool savePreferences() {
+    var prefs = Preferences()
+      ..darkMode = darkMode
+      ..language = language
+      ..theme = theme;
 
-  var io = IO();
-  io.putUserPreferences(data:prefs);
-  return true;
+    var io = IO();
+    io.putUserPreferences(data: prefs);
+    return true;
   }
 
-  //Publikus függvények
-  void intialize () async {
-    ownSharedPreferences??= await SharedPreferences.getInstance();
+  //Privát függvények
+  void _intialize() async {
+    _ownSharedPreferences ??= await SharedPreferences.getInstance();
   }
-
 }
