5b85f39a43eeabe244f77a9e3abc015d25ed0d57
Hertelendy Barnabás <barnabas.hertelendy@gmail.com>
2021-04-03 22:37:49 +0200

    Firebase SignIn és logout függvények megírva. Saját SignIn függvény elkezdve.

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:szikapp/utils/user.dart' as user;
+
+import 'io.dart';
 
 class Auth {
   //Kell még:
@@ -8,36 +11,45 @@ class Auth {
   // Visszatérni User-, vagy User.guest-tel.
 
   final _auth = FirebaseAuth.instance;
-
-  Future<UserCredential> signInWithUserCredential(AuthCredential credential) =>
-      _auth.signInWithCredential(credential);
+  user.User? _user;
 
   Future<void> logout() => _auth.signOut();
 
-  Stream<User> get currentUser => _auth.authStateChanges();
-}
+  Stream<User?> get stateChanges => _auth.authStateChanges();
 
-class AuthBloc {
-  final auth = Auth();
-  final googleSignin = GoogleSignIn(scopes: ['email']);
+  User? get currentUser => _auth.currentUser;
 
-  Stream<User> get currentUser => auth.currentUser;
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  loginGoogle() async {
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    ) as GoogleAuthCredential;
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future<bool?> signIn() async {
+    await signInWithGoogle();
     try {
-      final GoogleSignInAccount googleUser = googleSignin.signIn();
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken,
-        accessToken: googleAuth.accessToken,
-      );
-
-      // Firebase Sign in
-      final result = await auth.signInWithUserCredential(credential);
-      print('${result.user.displayName}')
-    } catch (error) {
-      print(error);
+      var io = IO();
+      await io.getUser(null);
+    } on Exception catch (e) {
+      _user =
+          user.User.guest(email: email); // emailt kiszedni a currentUser-ből
+      return true;
     }
+  }
+
+  Future<String> getAuthToken([bool forceRefresh = false]) {
+    return _delegate.getIdToken(forceRefresh);
   }
 }
