b98502889e5c0620f1b13445722cefaced627b8b
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2021-04-12 15:31:03 +0200

    Felső szintű navigáció hozzáadása

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 ///[SZIKApp] is an awesome application made in Flutter for the lovely people
 ///in da SZIK.
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(SZIKApp());
+import 'pages/home_page.dart';
+import 'pages/menu_page.dart';
+
+const routeHome = '/';
+const routeSettings = '/settings';
+const routeMenu = '/menu';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('hu')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('hu'),
+      child: SZIKApp(),
+      useOnlyLangCode: true,
+    ),
+  );
 }
 
 class SZIKApp extends StatefulWidget {
+  SZIKApp({Key key = const Key('SzikApp')}) : super(key: key);
+
   @override
   _SZIKAppState createState() => _SZIKAppState();
 }
@@ -14,6 +34,31 @@ class SZIKApp extends StatefulWidget {
 class _SZIKAppState extends State<SZIKApp> {
   @override
   Widget build(BuildContext context) {
-    return Text('Hello world!');
+    return MaterialApp(
+      title: 'SzikApp',
+      initialRoute: routeHome,
+      onGenerateRoute: _onGenerateRoute,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+    );
+  }
+
+  Route<dynamic>? _onGenerateRoute(RouteSettings settings) {
+    late Widget page;
+    if (settings.name == routeHome) {
+      page = HomePage();
+    } else if (settings.name == routeMenu) {
+      page = MenuPage();
+    } else {
+      throw Exception(
+          'NAVIGATOR_MESSAGE_ERROR'.tr(args: [settings.name ?? '']));
+    }
+    return MaterialPageRoute<dynamic>(
+      builder: (context) {
+        return page;
+      },
+      settings: settings,
+    );
   }
 }
