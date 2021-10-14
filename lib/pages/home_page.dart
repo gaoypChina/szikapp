b1a1f88cc0f0f65b5b82f0227f229bb81464d19f
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2021-10-14 11:44:34 +0200

    Profile deeplink hozzáadása

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -105,6 +105,11 @@ class _HomePageState extends State<HomePage> {
       final Uri? deepLink = dynamicLink?.link;
 
       if (deepLink != null) {
+        if (deepLink.path == '/me') {
+          pushNewScreen(context,
+              screen: SZIKAppState.getDestination(
+                  const RouteSettings(name: '/profile')));
+        }
         pushNewScreen(
           context,
           screen:
@@ -122,6 +127,11 @@ class _HomePageState extends State<HomePage> {
     final Uri? deepLink = data?.link;
 
     if (deepLink != null) {
+      if (deepLink.path == '/me') {
+        pushNewScreen(context,
+            screen: SZIKAppState.getDestination(
+                const RouteSettings(name: '/profile')));
+      }
       pushNewScreen(
         context,
         screen: SZIKAppState.getDestination(RouteSettings(name: deepLink.path)),
