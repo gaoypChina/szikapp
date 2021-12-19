31dc9ccc41e0c5f23a8531fdd9362fb6c04c1e8e
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2021-12-19 13:33:31 +0100

    Profil képernyő dizájn

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:szikapp/main.dart';
+
+import '../main.dart';
+import '../ui/widgets/profile_fields.dart';
 
 class ProfilePage extends StatelessWidget {
   static const String route = '/profile';
@@ -9,10 +10,18 @@ class ProfilePage extends StatelessWidget {
 
   const ProfilePage({Key key = const Key('ProfilePage')}) : super(key: key);
 
+  String buildGroupNamesFromIDs(List<String> ids) {
+    var result = '';
+    for (var item in ids) {
+      result +=
+          SZIKAppState.groups.firstWhere((element) => element.id == item).name;
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //ide majd Scaffoldwithheader kell, de valamiért ebből a branchből nem látszott
       resizeToAvoidBottomInset: true,
       body: Container(
         color: Theme.of(context).colorScheme.background,
@@ -21,207 +30,77 @@ class ProfilePage extends StatelessWidget {
           children: [
             Container(
               alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/pictures/default.png'),
-                    fit: BoxFit.contain),
-                borderRadius: BorderRadius.all(Radius.circular(50)),
+              child: CircleAvatar(
+                radius: 40,
+                foregroundImage: NetworkImage(
+                  SZIKAppState.authManager.user!.profilePicture.toString(),
+                ),
               ),
             ),
             Form(
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_NAME'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline3!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue: SZIKAppState.authManager.user!.name,
-                              readOnly: true,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  ProfileTextField(
+                    label: 'PROFILE_NAME'.tr(),
+                    initialValue: SZIKAppState.authManager.user!.name,
+                    readOnly: true,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_NICKNAME'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline3!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue: SZIKAppState.authManager.user!.nick,
-                              readOnly: false,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_EMAIL'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline3!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue:
-                                  SZIKAppState.authManager.user!.email,
-                              readOnly: true,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  ProfileTextField(
+                    label: 'PROFILE_NICKNAME'.tr(),
+                    initialValue: SZIKAppState.authManager.user!.nick,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_BIRTHDAY'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline2!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue: SZIKAppState.authManager.user!
-                                  .toString(), //itt a birthdaynek kell lenni, de nem tudtam a castolást hogy kell intézni:(
-                              readOnly: true,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_PHONENUMBER'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline2!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue:
-                                  SZIKAppState.authManager.user!.phone,
-                              readOnly: false,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  ProfileTextField(
+                    label: 'PROFILE_EMAIL'.tr(),
+                    initialValue: SZIKAppState.authManager.user!.email,
+                    readOnly: true,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'PROFILE_GROUPS'.tr(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline2!
-                              .copyWith(
-                                  color: Theme.of(context).colorScheme.primary),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 30.0),
-                            child: TextFormField(
-                              initialValue: SZIKAppState.authManager.user!
-                                  .toString(), //itt pedig a GroupID-knak vagyis a neveiknek kellene állnia, ezzel sem boldogultam
-                              readOnly: true,
-                            ),
-                          ),
-                        ),
-                        Divider(
-                            indent: 0,
-                            endIndent: 10,
-                            height: 1,
-                            thickness: 4,
-                            color: Theme.of(context).colorScheme.primary),
-                      ],
-                    ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  ProfileTextField(
+                    label: 'PROFILE_BIRTHDAY'.tr(),
+                    initialValue: SZIKAppState.authManager.user!.birthday !=
+                            null
+                        ? DateFormat('yyyy. MM. dd.')
+                            .format(SZIKAppState.authManager.user!.birthday!)
+                        : null,
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  ProfileTextField(
+                    label: 'PROFILE_PHONENUMBER'.tr(),
+                    initialValue: SZIKAppState.authManager.user!.phone,
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  ProfileTextField(
+                    label: 'PROFILE_GROUPS'.tr(),
+                    initialValue: buildGroupNamesFromIDs(
+                        SZIKAppState.authManager.user!.groupIDs ?? []),
+                    readOnly: true,
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
                 ],
               ),
