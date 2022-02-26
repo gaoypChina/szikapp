db2857b1d92c2c4e0c9fdf4f2cd6230c7a039e31
sarvarimark <50302858+sarvarimark@users.noreply.github.com>
2022-02-26 17:54:46 +0100

    Visszaszámlálás + widget kezdetek

--- lib/screens/poll_screen.dart
+++ lib/screens/poll_screen.dart
@@ -2,15 +2,11 @@ import 'dart:core';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:toggle_list/toggle_list.dart';
 
 import '../business/poll_manager.dart';
 import '../components/components.dart';
-import '../main.dart';
 import '../models/models.dart';
 import '../ui/themes.dart';
-import '../utils/utils.dart';
 
 class PollScreen extends StatelessWidget {
   static const String route = '/poll';
@@ -50,18 +46,7 @@ class PollTileView extends StatefulWidget {
 }
 
 class _PollTileViewState extends State<PollTileView> {
-  DateTime d1 = DateTime.utc(2020, 2, 17);
-  DateTime d2 = DateTime.utc(2020, 2, 18);
-  DateTime d3 = DateTime.utc(2020, 2, 20);
-  DateTime d4 = DateTime.utc(2020, 2, 23);
-  //PollTask(uid: '0', name: 'Szav1', start: , end: end, type: type, lastUpdate: lastUpdate, question: question, answerOptions: answerOptions, answers: answers, issuerIDs: issuerIDs)
-
   List<PollTask> _polls = [];
-/*
-    PollTask(uid : '0', name : 'Szav1', start : DateTime.utc(2020, 2, 17) , end : DateTime.utc(2020, 2, 18), type : TaskType.poll, lastUpdate : DateTime.utc(2020, 2, 17), question : 'Szavazás 1', answerOptions : ['1. opció','2. opció'], answers: [], issuerIDs : ['u999']),
-    PollTask(uid : '1', name : 'Szav2', start : DateTime.utc(2020, 2, 20) , end : DateTime.utc(2020, 2, 23), type : TaskType.poll, lastUpdate : DateTime.utc(2020, 2, 17), question : 'Szavazás 1', answerOptions : ['1. opció','2. opció'], answers: [], issuerIDs : ['u999']),
-*/
-
   bool _isActivePolls = true;
 
   @override
@@ -91,46 +76,51 @@ class _PollTileViewState extends State<PollTileView> {
           Expanded(
             child: GridView.count(
               padding: const EdgeInsets.fromLTRB(
-                  kPaddingNormal, kPaddingLarge, kPaddingNormal, 0),
+                kPaddingNormal,
+                kPaddingLarge,
+                kPaddingNormal,
+                0,
+              ),
               crossAxisCount:
                   MediaQuery.of(context).orientation == Orientation.landscape
                       ? 4
                       : 2,
               crossAxisSpacing: kPaddingNormal,
               mainAxisSpacing: kPaddingNormal,
-              children: _polls.map<Container>((poll) {
-                var difference = poll.end.difference(DateTime.now()).inDays;
-
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(kBorderRadiusNormal),
-                    color: poll.isLive
-                        ? theme.colorScheme.primary
-                        : theme.colorScheme.secondaryContainer,
+              children: _polls.map<GestureDetector>((poll) {
+                return GestureDetector(
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) {
+                      return const PollWidget();
+                    },
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(kBorderRadiusNormal),
-                    child: Column(
-                      children: [
-                        Expanded(
-                            child: Text(
-                          poll.question,
-                          style: theme.textTheme.subtitle1?.copyWith(
-                            color: theme.colorScheme.surface,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(kBorderRadiusNormal),
+                      color: poll.isLive
+                          ? theme.colorScheme.primary
+                          : theme.colorScheme.secondaryContainer,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(kBorderRadiusNormal),
+                      child: Column(
+                        children: [
+                          Expanded(
+                              child: Text(
+                            poll.question,
+                            style: theme.textTheme.subtitle1?.copyWith(
+                              color: theme.colorScheme.surface,
+                            ),
+                          )),
+                          Text(
+                            _calculateTime(poll.end),
+                            style: theme.textTheme.subtitle1?.copyWith(
+                              color: theme.colorScheme.primaryContainer,
+                            ),
                           ),
-                        )),
-                        Text(
-                          'POLL_DAYS_LEFT'.tr(
-                            args: [
-                              (poll.end.difference(DateTime.now()).inDays)
-                                  .toString()
-                            ],
-                          ),
-                          style: theme.textTheme.subtitle1?.copyWith(
-                            color: theme.colorScheme.primaryContainer,
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -149,4 +139,28 @@ class _PollTileViewState extends State<PollTileView> {
   }
 
   void _onCreatePoll() {}
+
+  String _calculateTime(DateTime date) {
+    var difference = date.difference(DateTime.now());
+    var answer = '';
+    if (difference.isNegative) {
+      answer = 'POLL_NO_TIME_LEFT'.tr();
+    } else if (difference.inDays > 0) {
+      answer = 'POLL_DAYS_LEFT'.tr(args: [difference.inDays.toString()]);
+    } else if (difference.inHours > 0) {
+      answer = 'POLL_HOURS_LEFT'.tr(args: [difference.inHours.toString()]);
+    } else if (difference.inMinutes > 0) {
+      answer = 'POLL_MINUTES_LEFT'.tr(args: [difference.inMinutes.toString()]);
+    }
+    return answer;
+  }
+}
+
+class PollWidget extends StatelessWidget {
+  const PollWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
