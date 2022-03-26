a4010c48874e081fa67ca3a4e7b77659363c5c78
P0T00 <74874100+P0T00@users.noreply.github.com>
2022-03-26 10:58:51 +0100

    gdpr: warning némítás / szebb kód

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -7,11 +7,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:provider/provider.dart';
-import 'package:szikapp/components/gdpr_widget.dart';
 
 import '../business/auth_manager.dart';
 import '../business/settings.dart';
 import '../components/components.dart';
+import '../components/gdpr_widget.dart';
 import '../main.dart';
 import '../navigation/app_state_manager.dart';
 import '../utils/utils.dart';
