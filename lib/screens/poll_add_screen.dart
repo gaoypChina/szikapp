0bad89a8e3dc78f0df71d75c359db54b82de9dd2
ksolyom <solyom1045@gmail.com>
2022-04-03 19:12:32 +0200

    poll add screen 04.03 sprint

--- lib/screens/poll_add_screen.dart
+++ lib/screens/poll_add_screen.dart
@@ -54,101 +54,284 @@ class _PollAddViewState extends State<PollAddView> {
     var theme = Theme.of(context);
     return CustomScaffold(
       appBarTitle: 'POLL_TITLE'.tr(),
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(kBorderRadiusNormal),
-                      color: theme.colorScheme.primary),
-                  child: Text(
-                    'POLL_ADD'.tr(),
-                    style: theme.textTheme.subtitle1
-                        ?.copyWith(color: theme.colorScheme.surface),
-                  ),
+      body: Container(
+        clipBehavior: Clip.hardEdge,
+        margin: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.surface,
+          borderRadius: BorderRadius.circular(kBorderRadiusNormal),
+          border: Border.all(
+            color: theme.colorScheme.primary,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.8),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(4, 4), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            //fejlélc
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primary,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'POLL_ADD'.tr(),
+                  style: theme.textTheme.subtitle1?.copyWith(
+                      color: theme.colorScheme.surface,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
                 ),
-                //kérdés
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'POLL_WRITE_QUESTION'.tr(),
-                  ),
-                  maxLines: 2,
-                  minLines: 1,
-                ),
-                //szavazás leírása
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'POLL_DESCRIPTION'.tr(),
-                  ),
-                  maxLines: 5,
-                  minLines: 1,
-                ),
-                //választási lehetőségek
-                Text(
-                  'POLL_OPTIONS'.tr(),
-                  style: theme.textTheme.subtitle1
-                      ?.copyWith(color: theme.colorScheme.primaryContainer),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: '...',
-                  ),
-                ),
+              ),
+            ),
 
-                FloatingActionButton(
-                  //középre
-                  onPressed: _onAddOption,
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints.expand(width: 36, height: 36),
-                    child: Image.asset('assets/icons/plus_light_72.png'),
-                  ),
-                ),
-
-                Text(
-                  'POLL_DURATION'.tr(),
-                  style: theme.textTheme.subtitle1
-                      ?.copyWith(color: theme.colorScheme.primaryContainer),
-                ),
-
-                //dátumok kiválasztása
-                TextFormField(
-                    //controller: dateCtl,
-                    decoration: const InputDecoration(
-                        //hintText: '',
+            //tartalom
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(kPaddingNormal),
+                child: ListView(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusNormal),
+                          ),
+                          hintText: 'POLL_WRITE_QUESTION'.tr(),
                         ),
-                    onTap: () async {
-                      DateTime? date = DateTime(1900);
-                      FocusScope.of(context).requestFocus(FocusNode());
-
-                      date = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(1900),
-                        lastDate: DateTime(2100),
-                      );
-                    }
-                    //dateCtl.text = date.toIso8601String();},)
+                        maxLines: 2,
+                        minLines: 1,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    //szavazás leírása
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusNormal),
+                          ),
+                          hintText: 'POLL_DESCRIPTION'.tr(),
+                        ),
+                        maxLines: 5,
+                        minLines: 1,
+                      ),
+                    ),
+                    //választási lehetőségek
+                    Container(
+                      margin: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Text(
+                        'POLL_OPTIONS'.tr(),
+                        style: theme.textTheme.subtitle1?.copyWith(
+                            color: theme.colorScheme.primaryContainer),
+                      ),
                     ),
 
-                Text(
-                  'POLL_PARTICIPANTS'.tr(),
-                  style: theme.textTheme.subtitle1
-                      ?.copyWith(color: theme.colorScheme.primaryContainer),
-                ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusNormal),
+                          ),
+                          hintText: 'POLL_ANSWER_OPTION'.tr(),
+                        ),
+                      ),
+                    ),
 
-                SearchableOptions<Group>(
-                    items:
-                        Provider.of<SzikAppStateManager>(context, listen: false)
+                    const SizedBox(height: 10),
+
+                    SizedBox(
+                      height: 30.0,
+                      width: 30.0,
+                      child: FittedBox(
+                        child: FloatingActionButton(
+                          onPressed: _onAddOption,
+                          child: ConstrainedBox(
+                            constraints: const BoxConstraints.expand(
+                                width: 36, height: 36),
+                            child:
+                                Image.asset('assets/icons/plus_light_72.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Text(
+                        'POLL_DURATION'.tr(),
+                        style: theme.textTheme.subtitle1?.copyWith(
+                            color: theme.colorScheme.primaryContainer),
+                      ),
+                    ),
+
+                    //dátumok kiválasztása
+                    Expanded(
+                      child: TextFormField(
+                        //controller: dateCtl,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusNormal),
+                          ),
+                          //hintText: '',
+                        ),
+                        onTap: () async {
+                          DateTime? date = DateTime(1900);
+                          FocusScope.of(context).requestFocus(FocusNode());
+
+                          date = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1900),
+                            lastDate: DateTime(2100),
+                          );
+                        },
+                        //dateCtl.text = date.toIso8601String();},)
+                      ),
+                    ),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Text(
+                        'POLL_PARTICIPANTS'.tr(),
+                        style: theme.textTheme.subtitle1?.copyWith(
+                            color: theme.colorScheme.primaryContainer),
+                      ),
+                    ),
+
+                    SearchableOptions<Group>(
+                        items: Provider.of<SzikAppStateManager>(context,
+                                listen: false)
                             .groups,
-                    onItemChanged: _onItemChanged,
-                    compare: (i, s) => i!.isEqual(s))
-              ],
+                        onItemChanged: _onItemChanged,
+                        compare: (i, s) => i!.isEqual(s)),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'POLL_SECRET'.tr(),
+                            style: theme.textTheme.subtitle1?.copyWith(
+                                color: theme.colorScheme.primaryContainer),
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Switch(
+                                value: true,
+                                onChanged: _onSecretPollChanged,
+                                activeColor: theme.colorScheme.primary),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'POLL_MULTIPLE_CHOICE'.tr(),
+                            style: theme.textTheme.subtitle1?.copyWith(
+                                color: theme.colorScheme.primaryContainer),
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Switch(
+                              value: true,
+                              onChanged: _onMultipleChoiceChanged,
+                              activeColor: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'POLL_MAX_OPTIONS'.tr(),
+                            style: theme.textTheme.subtitle1?.copyWith(
+                                color: theme.colorScheme.primaryContainer),
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              //itt mi történik?
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      kBorderRadiusNormal),
+                                ),
+                                hintText: '...',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      margin: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Text(
+                        'POLL_FEEDBACK'.tr(),
+                        style: theme.textTheme.subtitle1?.copyWith(
+                            color: theme.colorScheme.primaryContainer),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(kBorderRadiusNormal),
+                          ),
+                          hintText: 'POLL_THANKS'.tr(),
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //kuka ikon
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: FlatButton(
+                              child: Text(
+                                'POLL_CREATE'.tr(),
+                              ),
+                              color: theme.colorScheme.secondary,
+                              textColor: theme.colorScheme.surface,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -156,4 +339,8 @@ class _PollAddViewState extends State<PollAddView> {
   void _onAddOption() {}
 
   void _onItemChanged(Group? value) {}
+
+  void _onSecretPollChanged(bool value) {}
+
+  void _onMultipleChoiceChanged(bool value) {}
 }
