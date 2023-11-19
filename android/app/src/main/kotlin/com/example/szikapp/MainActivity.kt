e470954ac0309aeb3dba2245453d8cfbdb355bb2
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2023-11-20 00:03:15 +0100

    Update Android dependencies and configurations
    (cherry picked from commit fef5ffdb295c7744733a7cd8bce1f6e101f9624b)

--- android/app/src/main/kotlin/com/example/szikapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/szikapp/MainActivity.kt
@@ -4,8 +4,6 @@ import io.flutter.embedding.android.FlutterActivity;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.android.DrawableSplashScreen;
-import io.flutter.embedding.android.SplashScreen;
 
 import android.widget.ImageView;
 import android.graphics.drawable.Drawable;
@@ -15,28 +13,5 @@ import android.view.ViewTreeObserver
 import android.view.WindowManager
 
 class MainActivity: FlutterActivity() {
-    /*override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-
-    override fun provideSplashScreen(): SplashScreen? {
-        val manifestSplashDrawable = getSplashScreenFromManifest();
-        return DrawableSplashScreen(
-            manifestSplashDrawable,
-            ImageView.ScaleType.FIT_XY,
-            0 // Fade in duration
-        );
-    }
-
-    // Copied from FlutterActivity since it's private
-    private fun getSplashScreenFromManifest(): Drawable {
-        val activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA)
-        val metadata = activityInfo.metaData
-        val splashScreenId = metadata.getInt("io.flutter.embedding.android.SplashScreenDrawable")
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            return getResources().getDrawable(splashScreenId, getTheme())
-        } else {
-            return getResources().getDrawable(splashScreenId)
-        }
-    }*/
+    
 }
